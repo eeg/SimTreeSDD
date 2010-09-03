fd9da5cfc4f78e3467a5c53e9baa2e684210444c
Emma Goldberg <emma@skink>
2010-09-03 15:56:47 -0600

    make-dist.sh puts date in README

--- make-dist.sh
+++ make-dist.sh
@@ -8,7 +8,8 @@ if [ -a $ARCHIVE ]
 then
 	echo "aborting:" $ARCHIVE "already exists" 
 else
-	git checkout-index COPYING misc/* README src/* --prefix=$ARCHIVE/
+	git checkout-index COPYING misc/* src/* --prefix=$ARCHIVE/
+	echo -e  "$PACKAGE-$VERSION\n\n$(cat README)" > $ARCHIVE/README
 	cp doc/SimTreeSDD-manual.pdf $ARCHIVE/misc/
 	tar czvf $ARCHIVE.tar.gz $ARCHIVE/
 fi
