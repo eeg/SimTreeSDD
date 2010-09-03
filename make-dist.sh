7564293089407fba1024f9e1e62df2b91e9dc81a
Emma Goldberg <emma@skink>
2010-09-03 11:54:38 -0600

    script for creating archive for distribution

---
+++ make-dist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash          
+
+PACKAGE=SimTreeSDD
+VERSION=$(date +%Y%m%d)
+ARCHIVE=packaged/$PACKAGE-$VERSION
+
+if [ -a $ARCHIVE ]
+then
+	echo "aborting:" $ARCHIVE "already exists" 
+else
+	git checkout-index COPYING misc/* README src/* --prefix=$ARCHIVE/
+	cp doc/SimTreeSDD-manual.pdf $ARCHIVE/misc/
+	tar czvf $ARCHIVE.tar.gz $ARCHIVE/
+fi
